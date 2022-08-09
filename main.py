92518ddcdb25099a68aca32a12a78b8740a43621
Alvis Lai <59428579+AlvisLai@users.noreply.github.com>
2022-08-09 16:43:30 -0400

    add import

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#init
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
